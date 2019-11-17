5174be6cef16ea2cddc4a4d2d355e6df4d3fedf5
Aleksey Kravets <alekravets976@gmail.com>
2019-11-17 20:30:14 +0200

    work in lesson

--- function.py
+++ function.py
@@ -8,26 +8,26 @@ def G(s):
     if s[1] <= 0:
         return 0
     else:
-         return 1/(4*math.pi*(math.e**2)*s[1])**0.5 *math.exp((-1*s[0]**2)/(4*math.e*s[1]))
+        #  return 1/(4*math.pi*(math.e**2)*s[1])**0.5 *math.exp((-1*s[0]**2)/(4*math.e*s[1]))
 
-        # return 1/(2*(math.pi * s[1])**0.5) * math.exp(-( s[1] + (s[0]-s[1])**2 / (4* s[1] )))  # пример из методички
+        return 1/(2*(math.pi * s[1])**0.5) * math.exp(-( s[1] + (s[0]-s[1])**2 / (4* s[1] )))  # пример из методички
 
 
 def y(s: np.ndarray)-> float:
     res = 0
     #for i in range(s.shape[0]):
-    for i in range(2):
-        res+= (s[i] -0.5)**2
+    # for i in range(2):
+    #     res+= (s[i] -0.5)**2
     # res = s[1]**2 + s[0]**3
 
-    # res = s[0] + 1 + s[1]/4       # пример из методички
+    res = s[0] + 1 + s[1]/4       # пример из методички
     return res
 
 
 def U(s):
-     return 2*(s[1]-0.5)- 2*math.e**2
+    #  return 2*(s[1]-0.5)- 2*math.e**2
     #return 2*s[1] - 6*math.e**2*s[0]
-    # return  9/4 + s[1]/4 + s[0]  # пример из методички
+    return  9/4 + s[1]/4 + s[0]  # пример из методички
 
 
 if __name__ == '__main__':
